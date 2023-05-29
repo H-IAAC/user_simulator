276d9dd55eb4827522393f38d73cad724d2c644b
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2023-05-29 13:55:37 -0300

    Pause with multiples agents test

---
+++ User Simulator/Assets/Scripts/RevealHiddenObjects.cs
@@ -0,0 +1,51 @@
+//https://forum.unity.com/threads/gameobjects-deleted-or-invisible-in-hierarchy-but-accessible-by-code.831298/
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public static class RevealHiddenObjects
+{
+    [MenuItem("Tools/Reveal Hidden GameObjects")]
+    private static void RevealHiddenGameObjects()
+    {
+        //Reveal GO in all loaded scenes
+        int countLoaded = SceneManager.sceneCount;
+        for (int i = 0; i < countLoaded; i++)
+        {
+            var scene = SceneManager.GetSceneAt(i);
+            foreach(var gameObject in scene.GetRootGameObjects())
+            {
+                RevealHiddenGameObject(gameObject);
+            }
+
+            Debug.Log(scene.name);
+        }
+
+        //Reveal also GO in DontDestroyOnLoad scene
+        var temp = new GameObject();
+        Object.DontDestroyOnLoad( temp );
+        Scene dontDestroyOnLoad = temp.scene;
+        Object.DestroyImmediate( temp );
+        temp = null;
+        
+        foreach(var gameObject in dontDestroyOnLoad.GetRootGameObjects())
+        {
+            RevealHiddenGameObject(gameObject);
+        }
+
+    }
+ 
+    private static void RevealHiddenGameObject(GameObject gameObject)
+    {
+        if(gameObject.hideFlags.HasFlag(HideFlags.HideInHierarchy))
+        {
+            Debug.Log("Revealing hidden GameObject "+gameObject.name, gameObject);
+            gameObject.hideFlags &= ~HideFlags.HideInHierarchy;
+        }
+ 
+        foreach(Transform child in gameObject.transform)
+        {
+            RevealHiddenGameObject(child.gameObject);
+        }
+    }
+}
